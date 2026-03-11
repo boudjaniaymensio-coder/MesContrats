7d0c6973523cb486fbed5280dd3b5e897953c21c
boudjani_a <boudjani_a@D202-022024-08>
2026-03-11 09:00:36 +0100

    Ajoutez des fichiers projet.

---
+++ Categorie.cs
@@ -0,0 +1,25 @@
+﻿namespace MesContrats
+{
+    public class Categorie
+    {
+        private string _categLoc;
+        private string description;
+        private double _mtnCaution;
+        private double _tarifjour;
+        public Categorie(string categLoc, string description, double mtnCaution, double tarifjour)
+        {
+            this._categLoc = categLoc;
+            this.description = description;
+            this._mtnCaution = mtnCaution;
+            this._tarifjour = tarifjour;
+        }
+        public override string ToString()
+        {
+            return 
+
+        }
+
+
+
+    }
+}
---
+++ MesContrats/Categorie.cs
@@ -0,0 +1,10 @@
+﻿namespace MesContrats
+{
+    public class Categorie
+    {
+        private string _categLoc;
+        private string description;
+        private double _mtnCaution;
+        private double _tarifjour;
+    }
+}
